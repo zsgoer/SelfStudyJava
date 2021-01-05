688ad44018573428c1ee3be88b12894f5d3aecdc
orez <orez@DESKTOP-FPBOGOJ>
2021-01-05 23:59:02 +0900

    강제 타입 변환 - 큰 허용범위 변수에서 작은 허용범위 변수로 값을 넣을 떄 강제 타입변환으로 가능하다. ex ) int a = (int) longValue;

---
+++ src/chap02/sec03/exam02/CastingExample.java
@@ -0,0 +1,17 @@
+package chap02.sec03.exam02;
+
+public class CastingExample {
+	public static void main(String[] args) {
+		int intValue = 44032;
+		char charValue = (char) intValue;
+		System.out.println("charValue : " + charValue);
+
+		long longValue = 500;
+		intValue = (int) longValue;
+		System.out.println("intValue : " + intValue);
+
+		double doubleValue = 3.14;
+		intValue = (int) doubleValue;
+		System.out.println("intValue : " + intValue);
+	}
+}
