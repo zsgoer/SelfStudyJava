92f4028667fd2db59b25b1c75773a03feaa67092
orez <orez@DESKTOP-FPBOGOJ>
2021-01-06 00:07:46 +0900

    타입이 다른 정수를 연산 할 때 가장 큰 타입의 변수로 결과값을 받아줘야 한다.(작은 타입들이 가장 큰타입으로 변환되어 연산되기 때문이다.)

---
+++ src/chap02/sec03/exam04/LongOperationExample.java
@@ -0,0 +1,13 @@
+package chap02.sec03.exam04;
+
+public class LongOperationExample {
+
+	public static void main(String[] args) {
+		byte value1= 10;
+		int value2 = 100;
+		long value3 = 1000L;
+		long result = value1+ value2 + value3;
+		System.out.println(result);
+	}
+
+}
